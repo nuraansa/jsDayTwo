5748d778257d0df125d3eb75a3ed031c9812cf52
nuraansa <nuraansa@gmail.com>
2023-05-16 16:14:34 +0200

    first commit

---
+++ intro_js/script/code.js
@@ -0,0 +1,50 @@
+//this is my first js statement
+// console.log('Hello World');
+// document.write("My name is Nuraan");
+//document.getElementById('p1').innerText ='I Love Programming';
+//document.querySelector('#p1').textContent = 'I love JS!';
+// function displayContent(){
+//     document.querySelector('#p1').textContent = 'I love JS!';
+// }
+// let display = document.querySelector('#btnDisplay');
+// display.addEventListener('click', displayContent);
+
+// let firstName = "Nuraan";
+// let age  = 23;
+// console.log ('I am $[firstName] and I am $[age] years old');
+
+// let numb1 = prompt('Enter number 1')
+// let numb2 = prompt('Enter number 2')
+// let addition = numb1 + numb2;
+// console.log('Sum: ${addition}');
+// const isMale = true;
+
+// let subjects = ['C++', 'C#', 'Python', 'Javascript', 'nuri', 45, 12];
+// console.log (subjects);
+// console.log(subjects.length);
+
+// let personDetail ={
+//     firstName: ('Nuraan'),
+//     surname: ('Davids'),
+//     age: ('23')
+// };
+// console.log(personDetail);
+// console.log(personDetail.firstName);
+// console.log(personDetail['surname']);
+// console.log(personDetail.age);
+
+// // Destructuring
+// let {firstName, surname, age} = personDetail;
+// console.log(firstName);
+// console.log(surname);
+// console.log(age);
+
+let myName =('Nuraan');
+let mySurname =('Davids');
+console.log(myName+mySurname);
+
+let numberVar =(5);
+console.log(numberVar*5);
+
+const me = ("Nuraan");
+const me = ("Davids");
---
+++ operators/script/code.js
@@ -0,0 +1,45 @@
+//Arithmetic Operators: +, /, *, **, and %
+// let numb1 = 5;
+// let numb2 = 3;
+// console.log(numb1 % numb2);
+
+//Assignment Operators: +=, -=, /=, *=, %=,
+// let numb1 = 5;
+// numb1 -= 2;
+// console.log(numb1);
+
+//Logical Operators: &&, ||, ! 
+//&& AND
+// let x = 3;
+// let y = 5;
+// console.log( (3 > 5) && (3 < 5) );
+
+// || OR
+// let x = 6;
+// let y = 3;
+// console.log((x == 2) || (x == 6)); //true
+// console.log((x == 1) || (y == 6)); //false
+
+// /! NOT 
+// let x = 3;
+// let y = 5;
+// console.log(!(3 < 5)); //false
+
+//Comparison: <, <=, >=, >, !=, !==, ==, ===
+// let x = 3;
+// let y = 5;
+// console.log(x <= y);
+
+//If statement 
+// let salary = 5020;
+// let bonus = 500;
+// if (salary > 5000){
+//     console.log(salary + bonus);
+// } else {
+//     console.log(salary);
+// }
+
+//Ternary Operator ? :
+// let result = salary > 5000 ? salary + bonus : salary;
+// console.log(result);
+
